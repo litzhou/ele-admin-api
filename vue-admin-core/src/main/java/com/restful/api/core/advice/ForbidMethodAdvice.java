c414d70bc30a6180844aeec696809cce9915de0b
foreyer <274410087@qq.com>
2018-01-26 14:58:18 +0800

    init2

---
+++ vue-admin-core/src/main/java/com/restful/api/core/advice/ForbidMethodAdvice.java
@@ -0,0 +1,54 @@
+package com.restful.api.core.advice;
+
+import java.lang.reflect.Method;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import com.restful.api.core.anno.ForbidMethod;
+import com.restful.api.core.ex.ForbidAccessException;
+/**
+ * 禁止指定方法访问
+ * Created by Gaojun.Zhou 2017年6月21日
+ */
+@Aspect
+@Component
+public class ForbidMethodAdvice {
+	
+	public static final Logger logger = Logger.getLogger(ForbidMethodAdvice.class);
+	
+	@Pointcut("this(com.restful.api.core.controller.CrudController)")
+	public void controllerAspect() {
+		
+	}
+	/**
+	 * 当方法开始执行之前执行
+	 * @param joinPoint
+	 */
+	@Before("controllerAspect()")
+	public void doBefore(JoinPoint joinPoint) {
+		
+		MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+		Method method = methodSignature.getMethod();
+		
+		String name =  method.getName();	//方法名称
+		Object ctr = joinPoint.getTarget(); //当前控制器
+		
+		logger.debug("methodName : " + name + ", ctr : " + ctr);
+		
+		ForbidMethod forbidMethodAnno =  joinPoint.getTarget().getClass().getAnnotation(ForbidMethod.class);
+		if(forbidMethodAnno!=null){
+			String[] value = forbidMethodAnno.value();
+			if(ArrayUtils.contains(value, name)){
+				throw new ForbidAccessException(name+"禁止访问");
+			}
+		}
+		
+	}
+}
