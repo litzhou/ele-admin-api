1ed358ea5105d8340d2c99a21db7777418d2bca4
foreyer <274410087@qq.com>
2018-02-03 11:40:14 +0800

    update

--- src/main/java/com/vacomall/controller/sys/MenuController.java
+++ src/main/java/com/vacomall/controller/sys/MenuController.java
@@ -1,6 +1,5 @@
 package com.vacomall.controller.sys;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -9,10 +8,10 @@ import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.baomidou.mybatisplus.entity.Column;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
-import com.google.common.collect.Maps;
 import com.vacomall.common.anno.Log;
 import com.vacomall.common.bean.Rest;
 import com.vacomall.common.controller.SuperController;
@@ -74,86 +72,32 @@ public class MenuController extends SuperController {
 	}
 
 	/**
-	 * 增加菜单
-	 */
-	@RequiresPermissions("addMenu")
-	@RequestMapping("/add")
-	public String add(Model model) {
-
-		EntityWrapper<SysMenu> ew = new EntityWrapper<SysMenu>();
-		ew.orderBy("code", true);
-		ew.eq("pid", "0");
-		List<SysMenu> list = sysMenuService.selectList(ew);
-		model.addAttribute("list", list);
-		return "system/menu/add";
-
-	}
-
-	/**
 	 * 添加目录
 	 */
 	@RequiresPermissions("addMenu")
 	@Log("创建目录菜单")
-	@RequestMapping("/doAddDir")
-	@ResponseBody
-	public Rest doAddDir(SysMenu sysMenu, Model model) {
-
-		sysMenu.setPid("0");
-		sysMenu.setDeep(1);
-		sysMenuService.insert(sysMenu);
-		return Rest.ok();
-	}
-
-	/**
-	 * 添加菜单
-	 */
-	@RequiresPermissions("addMenu")
-	@Log("创建菜单")
-	@RequestMapping("/doAddMenu")
-	@ResponseBody
-	public Rest doAddMenu(SysMenu sysMenu, Model model) {
-		sysMenu.setDeep(2);
-		sysMenuService.insert(sysMenu);
-		return Rest.ok();
-	}
-
-	/**
-	 * 编辑菜单
-	 */
-	@RequiresPermissions("editMenu")
-	@RequestMapping("/edit/{id}")
-	public String edit(@PathVariable String id, Model model) {
-		SysMenu sysMenu = sysMenuService.selectById(id);
-		model.addAttribute("sysMenu", sysMenu);
-
-		if (sysMenu.getDeep() == 1) {
-			return "/system/menu/edit";
-		} else if (sysMenu.getDeep() == 2) {
-			EntityWrapper<SysMenu> ew = new EntityWrapper<SysMenu>();
-			ew.orderBy("code", true);
-			ew.eq("pid", "0");
-			List<SysMenu> list = sysMenuService.selectList(ew);
-			model.addAttribute("list", list);
-			return "/system/menu/editMenu";
+	@PostMapping("/add")
+	public Rest add(SysMenu sysMenu, @RequestParam(value = "pids[]", required = false) String[] pids) {
+		if (ArrayUtils.isEmpty(pids)) {
+			sysMenu.setPid("0");
+			sysMenu.setDeep(1);
 		} else {
-			EntityWrapper<SysMenu> ew = new EntityWrapper<SysMenu>();
-			ew.orderBy("code", true);
-			ew.eq("pid", "0");
-			List<SysMenu> list = sysMenuService.selectList(ew);
-			model.addAttribute("list", list);
-			model.addAttribute("pSysMenu", sysMenuService.selectById(sysMenu.getPid()));
-			return "/system/menu/editAction";
+			String pid = pids[pids.length - 1];
+			sysMenu.setPid(pid);
+			SysMenu sMenu = sysMenuService.selectById(pid);
+			sysMenu.setDeep(sMenu != null ? sMenu.getDeep()+1 : 0);
 		}
+		sysMenuService.insert(sysMenu);
+		return Rest.ok();
 	}
-
 	/**
 	 * 执行编辑菜单
 	 */
 	@RequiresPermissions("editMenu")
 	@Log("编辑菜单")
-	@RequestMapping("/doEdit")
+	@PutMapping("/edit")
 	@ResponseBody
-	public Rest doEdit(SysMenu sysMenu, Model model) {
+	public Rest doEdit(SysMenu sysMenu, @RequestParam(value = "pids[]", required = false) String[] pids) {
 		sysMenuService.updateById(sysMenu);
 		return Rest.ok();
 	}
@@ -173,28 +117,8 @@ public class MenuController extends SuperController {
 	}
 
 	/**
-	 * 根据父节点获取子菜单
-	 */
-	@RequestMapping("/json")
-	@ResponseBody
-	public Rest json(String pid) {
-		EntityWrapper<SysMenu> ew = new EntityWrapper<SysMenu>();
-		ew.orderBy("sort");
-		ew.addFilter("pid = {0} ", pid);
-		List<SysMenu> list = sysMenuService.selectList(ew);
-
-		List<Map<String, Object>> listMap = new ArrayList<Map<String, Object>>();
-		for (SysMenu m : list) {
-			Map<String, Object> map = Maps.newHashMap();
-			map.put("id", m.getId());
-			map.put("text", StringUtils.join(m.getCode(), "-", m.getMenuName()));
-			listMap.add(map);
-		}
-		return Rest.okData(listMap);
-	}
-	
-	/**
 	 * 获取所有的一级菜单和二级菜单,vue
+	 * 
 	 * @param pid
 	 * @return
 	 */
@@ -203,45 +127,20 @@ public class MenuController extends SuperController {
 		EntityWrapper<SysMenu> ew = new EntityWrapper<SysMenu>();
 		ew.orderBy("sort");
 		ew.eq("pid", "0");
-		ew.setSqlSelect(Column.create().column("id"),Column.create().column("menuName"));
-		List<Map<String,Object>> listMap = sysMenuService.selectMaps(ew);
+		ew.setSqlSelect(Column.create().column("id"), Column.create().column("menuName"));
+		List<Map<String, Object>> listMap = sysMenuService.selectMaps(ew);
 		for (Map<String, Object> map : listMap) {
 			EntityWrapper<SysMenu> wrapper = new EntityWrapper<SysMenu>();
 			wrapper.orderBy("sort");
 			wrapper.eq("pid", map.get("id"));
-			wrapper.setSqlSelect(Column.create().column("id"),Column.create().column("menuName"));
+			wrapper.setSqlSelect(Column.create().column("id"), Column.create().column("menuName"));
 			List<Map<String, Object>> listMapeExt = sysMenuService.selectMaps(wrapper);
-			if(listMapeExt.size() > 0){
-				map.put("children",listMapeExt);
+			if (listMapeExt.size() > 0) {
+				map.put("children", listMapeExt);
 			}
 		}
 		return Rest.okData(listMap);
 	}
-
-	/**
-	 * 验证菜单资源名称是否存在
-	 */
-	@RequestMapping("/checkMenuResource")
-	@ResponseBody
-	public Rest checkMenuResource(String resource) {
-
-		List<SysMenu> list = sysMenuService
-				.selectList(new EntityWrapper<SysMenu>().addFilter("resource = {0}", resource));
-		if (list.size() > 0) {
-			return Rest.failure("资源已存在,请换一个尝试.");
-		}
-		return Rest.ok();
-	}
-
-	@RequiresPermissions("addMenu")
-	@Log("新增功能菜单")
-	@RequestMapping("/doAddAction")
-	public String doAddAction(SysMenu sysMenu, Model model) {
-		sysMenu.setDeep(3);
-		sysMenuService.insert(sysMenu);
-		return redirectTo("/system/menu/list/1.html");
-	}
-
 	/**
 	 * 根据上级ID查询所有下级接单
 	 * 
